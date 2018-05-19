5d8a8c49f2915236d9a5c9eaa4e40193f40ef487
Ashwin George Philip <ashwinphilip27@gmail.com>
2018-05-19 06:17:56 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(array, element) {
-  ['foo', ...array]
+  array = ['foo', ...array]
   return array
 }
 
